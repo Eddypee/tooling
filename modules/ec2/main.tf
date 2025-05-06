d3d6527e0047879ae6c5b6c3e10de66700b9f8cb
Eddypee <63885682+Eddypee@users.noreply.github.com>
2025-05-06 20:11:53 +0100

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,7 +6,7 @@ data "aws_vpc" "default" {
 # Data source for the latest Ubuntu 22.04 AMI
 data "aws_ami" "ubuntu22" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical's owner ID
+  owners      = ["222353955460"] # Canonical's owner ID
 
   filter {
     name   = "name"
@@ -15,7 +15,7 @@ data "aws_ami" "ubuntu22" {
 }
 
 # Security group allowing SSH from the internet (port 22) using the default VPC
-resource "aws_security_group" "ansible_sg" {
+resource "aws_security_group" "onyi-appserver" {
   name        = "ansible_security_group"
   description = "Allow SSH inbound traffic on port 22"
   vpc_id      = data.aws_vpc.default.id
@@ -36,7 +36,7 @@ resource "aws_security_group" "ansible_sg" {
   }
 
   tags = {
-    Name = "ansible-sg"
+    Name = "onyi-appserver"
   }
 }
 
@@ -46,7 +46,7 @@ resource "aws_instance" "sonarqube" {
   ami                         = data.aws_ami.ubuntu22.id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  vpc_security_group_ids      = [aws_security_group.ansible_sg.id]
+  vpc_security_group_ids      = [aws_security_group.onyi-appserver.id]
   associate_public_ip_address = true
 
   tags = {
