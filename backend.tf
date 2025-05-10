cbf597a1ee19787ae8d62d76cb354c92465987a4
Eddypee <63885682+Eddypee@users.noreply.github.com>
2025-05-10 18:04:40 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=0.12.0"
   backend "s3" {
     key            = "tooling/terraform.state"
-    bucket         = "class38-terraform-backend-bucket"
+    bucket         = "class38-terraform-backend-bucket-projrct-edna"
     region         = "us-east-2"
     dynamodb_table = "terraform-state-locking"
+    encrypt        = true
   }
 }
